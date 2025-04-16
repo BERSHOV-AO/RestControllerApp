92b9f35b7747467f03634b09e3034e7c868616e6
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-16 15:05:50 +0300

    Add config

---
+++ src/main/java/ru/kata/configuration/MyWebInitializer.java
@@ -0,0 +1,22 @@
+package ru.kata.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.swing.*;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
